f5855d94c42d0fd1745fa2971dad5ba893e7246e
Pavel <roigoci@gmail.com>
2026-05-11 00:12:06 +0200

    Add sentry.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { APP_INITIALIZER, ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, Router } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import * as Sentry from '@sentry/angular';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -9,6 +10,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    { provide: ErrorHandler, useValue: Sentry.createErrorHandler() },
+    { provide: Sentry.TraceService, deps: [Router] },
+    { provide: APP_INITIALIZER, useFactory: () => () => {}, deps: [Sentry.TraceService], multi: true },
   ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import * as Sentry from '@sentry/angular';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+Sentry.init({
+  dsn: 'https://98b35f4d7f54606f7eadc18a50e66f9b@o4507279451357184.ingest.de.sentry.io/4511367880179792',
+  sendDefaultPii: true,
+});
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
